1e710922b85b47d1a94f6e867c22eba4af2f6a29
thinhld9xvn <thinhld9xvn@gmail.com>
2020-11-16 21:40:46 +0700

    module tạo sitemap có cấu trúc

---
+++ core/modules/sitemap/config.php
@@ -0,0 +1,9 @@
+<?php 
+	global $sitemap_config;
+
+	/*$sitemap_config['excludes'] = array(
+
+		'post_types' => array('stm_careers', 'stm_service', 'stm_staff', 'stm_works', 'stm_testimonials', 'stm_vc_sidebar', 'stm_portfolio'),
+		'taxonomies' => array('tag', 'stm_service_category', 'stm_staff_category', 'stm_works_category', 'stm_testimonials_category', 'stm_portfolio_category')
+
+	);
